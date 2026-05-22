f7175e571a283304a7bf564ade24ebaf6105e667
Kim JinWoo <kjinwoo12@gmail.com>
2026-05-22 22:51:27 +0900

    bugfix : VilageDemo UI

--- Assets/Scripts/Map/DungeonEntryPortal.cs
+++ Assets/Scripts/Map/DungeonEntryPortal.cs
@@ -1,27 +1,83 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class DungeonEntryPortal : MonoBehaviour
 {
-    [SerializeField] private GameObject ui;
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] GameObject ui;
+
+    void Awake() => EnsureUiResolved();
+
+    void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (!other.CompareTag("Player"))
+            return;
+
+        if (!EnsureUiResolved())
         {
-            Time.timeScale = 0f;
-            UIManager.Instance.inventory.currentUI = UIType.DungeonEntry;
-            UIManager.Instance.inventory.playerProfile.SetActive(false);
-            UIManager.Instance.inventory.playerAttack.uiClicking = true;
-            ui.SetActive(true);
+            Debug.LogWarning(
+                "DungeonEntryPortal: DungeonPortal UI를 찾을 수 없습니다. Canvas가 씬에 있는지 확인하세요.",
+                this);
+            return;
+        }
+
+        if (!VillageDungeonEntryUi.ShowDungeonEntryMenu())
+            return;
+
+        Time.timeScale = 0f;
+
+        if (TryGetInventory(out var inventory))
+        {
+            inventory.currentUI = UIType.DungeonEntry;
+
+            if (inventory.playerAttack != null)
+                inventory.playerAttack.uiClicking = true;
         }
     }
 
     public void Back()
     {
         Time.timeScale = 1f;
-        UIManager.Instance.inventory.currentUI = UIType.None;
-        UIManager.Instance.inventory.playerProfile.SetActive(true);
-        UIManager.Instance.inventory.playerAttack.uiClicking = false;
-        ui.SetActive(false);
+        VillageDungeonEntryUi.HideDungeonEntryMenu();
+
+        if (TryGetInventory(out var inventory))
+        {
+            inventory.currentUI = UIType.None;
+
+            if (inventory.playerAttack != null)
+                inventory.playerAttack.uiClicking = false;
+        }
+    }
+
+    public bool EnsureUiResolved()
+    {
+        if (ui != null)
+            return true;
+
+        ui = FindDungeonPortalPanel();
+        return ui != null;
+    }
+
+    static GameObject FindDungeonPortalPanel()
+    {
+        var scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+        foreach (var root in scene.GetRootGameObjects())
+        {
+            foreach (var transform in root.GetComponentsInChildren<Transform>(true))
+            {
+                if (transform.name == "DungeonPortal")
+                    return transform.gameObject;
+            }
+        }
+
+        return null;
+    }
+
+    static bool TryGetInventory(out InventoryMain inventory)
+    {
+        inventory = null;
+        if (UIManager.Instance == null || UIManager.Instance.inventory == null)
+            return false;
+
+        inventory = UIManager.Instance.inventory;
+        return true;
     }
 }
